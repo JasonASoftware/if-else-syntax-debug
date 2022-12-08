059a7e7b4da4044c6f92af43bf2ad925d1c6e31d
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-08 16:58:25 -0500

    created js file

---
+++ debug.js
@@ -0,0 +1,18 @@
+//default bug code
+// function checkAlive (health) {
+//   if () {
+//     health < 0
+//     return false
+//   } else () {
+//     return true
+//   }
+// }
+
+//debugged code
+function checkAlive (health) {
+  if (health > 0){
+    return true
+  } else {
+    return false
+  }
+}
